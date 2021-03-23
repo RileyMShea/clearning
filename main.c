2304377ddbefa509cf91b3ceebad4cea556c97fc
Riley <rileymshea@gmail.com>
2021-03-22 21:31:31 -0400

    first lgtm
    Signed-off-by: Riley <rileymshea@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Pet{
+  char name[50];
+};
+
+struct Person{
+  char name[50];
+  struct Pet pet;
+};
+
+int person_type(struct Person person){
+
+
+  return 0;
+}
+
 
 int main(){
-printf("hello");
-printf("world");
-return 0;
+  char a[50] = "Riley";
+  int size =  sizeof a / sizeof *a;
+  for (int i = 0;i < size ;i++){
+  }
+  struct Person riley;
+  strcpy(riley.name, a);
+  printf("%s %s",a,a);
+
+  return 0;
 }
